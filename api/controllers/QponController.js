6f45a2b2521d56c12f789cf14aa7fe7d2ea529aa
Wong Tsz Shing <ShInGSon@WongdeMacBook-Pro.local>
2018-02-14 18:16:02 +0800

    update

--- api/controllers/QponController.js
+++ api/controllers/QponController.js
@@ -22,4 +22,54 @@ module.exports = {
       return res.json(qpons);
     });
   },
+  admin: function (req, res) {
+    Qpon.find().exec(function (err, qpons) {
+      return res.view('qpon/admin', {
+        'qpons': qpons
+      });
+    });
+  },
+  // delete function
+  delete: function (req, res) {
+    Qpon.findOne(req.params.id).exec(function (err, model) {
+      if (model != null) {
+        model.destroy();
+        return res.send("Qpon Deleted");
+      } else {
+        return res.send("Qpon not found");
+      }
+    });
+  },
+  // update function
+  update: function (req, res) {
+    if (req.method == "GET") {
+      Qpon.findOne(req.params.id).exec(function (err, model) {
+        if (model == null)
+          return res.send("No such Qpon!");
+        else
+          return res.view('qpon/update', {
+            'qpon': model
+          });
+      });
+    } else {
+      Qpon.findOne(req.params.id).exec(function (err, model) {
+        model.title = req.body.Qpon.title;
+        model.rest = req.body.Qpon.rest;
+        model.district = req.body.Qpon.district;
+        model.mall = req.body.Qpon.mall;
+        model.image = req.body.Qpon.image;
+        model.coin = req.body.Qpon.coin;
+        model.date = req.body.Qpon.date;
+        model.quota = req.body.Qpon.quota;
+        model.details = req.body.Qpon.details;
+        model.save();
+        return res.send("Record updated");
+      });
+    }
+  },
+  search: function (req, res) {
+
+    
+
+  },
 };
